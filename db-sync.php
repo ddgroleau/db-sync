ea599a5f5dc7d8c357df62f58d31816d2bd6e529
Dan Groleau <ddgroleau@gmail.com>
2024-07-27 15:42:18 -0400

    Update data structure for sync utility

--- db-sync.php
+++ db-sync.php
@@ -1,25 +1,39 @@
 <?php
+/* SETUP */
 $env = parse_ini_file('.env');
-$localDbHost = $env["LOCAL_DB_HOST"];
-$localDbUser = $env["LOCAL_DB_USER"];
-$localDbPassword = $env["LOCAL_DB_PASSWORD"];
-$serverDbHost = $env["SERVER_DB_HOST"];
-$serverDbUser = $env["SERVER_DB_USER"];
-$serverDbPassword = $env["SERVER_DB_PASSWORD"];
 $dumpDir = $env["DUMPS_FOLDER"];
 $databases = explode(",", $env["DATABASES"]);
+$environments = explode(",", $env["ENVIRONMENTS"]);
+
+$jsonInfo = file_get_contents("db-info.json");
+
+if ($jsonInfo === false) {
+    die("Failed to read JSON file.");
+}
+
+$dbInfo = json_decode($jsonInfo, true);
+
+if (json_last_error() !== JSON_ERROR_NONE) {
+    die("Failed to decode JSON: " . json_last_error_msg());
+}
+
+function generateBackupFileCommand(string $env, string $file): string
+{
+    global $database;
+    global $dbInfo;
+
+    $host = $dbInfo[$database][$env]["host"];
+    $user = $dbInfo[$database][$env]["user"];
+    $password = $dbInfo[$database][$env]["password"];
+    $escapedPassword = str_replace(" ", "!", escapeshellarg($password));
+
+    return "\"C:\\Program Files\\MySQL\\MySQL Workbench 8.0 CE\\mysqldump.exe\" --host=$host --port=3306 --default-character-set=utf8 --user=$user --password=$escapedPassword --protocol=tcp --skip-triggers --skip-column-statistics $database > $file";
+}
+/* END SETUP */
 
 
 /* CONFIG QUESTIONS */
-echo "Dry run? (No sync) (y/n): ";
 $handle = fopen("php://stdin", "r");
-$isDryRun = trim(fgets($handle));
-
-if (strtolower($isDryRun) !== 'y' && strtolower($isDryRun) !== 'n') {
-    die("Invalid input. Please enter 'y' or 'n'.");
-}
-
-$isDryRun = strtolower($isDryRun) === 'y';
 
 echo "Monthly backup? (y/n): ";
 $isMonthlyBackup = trim(fgets($handle));
@@ -29,11 +43,24 @@ if (strtolower($isMonthlyBackup) !== 'y' && strtolower($isMonthlyBackup) !== 'n'
 }
 
 $isMonthlyBackup = strtolower($isMonthlyBackup) === 'y';
+$isDryRun = false;
+
+if (!$isMonthlyBackup) {
+    echo "Dry run? (No sync) (y/n): ";
+    $isDryRun = trim(fgets($handle));
+
+    if (strtolower($isDryRun) !== 'y' && strtolower($isDryRun) !== 'n') {
+        die("Invalid input. Please enter 'y' or 'n'.");
+    }
+
+    $isDryRun = strtolower($isDryRun) === 'y';
+}
 
 echo "\n\nConfiguration:\n";
 echo "Dry Run: " . ($isDryRun ? "Yes" : "No") . "\n";
 echo "Monthly Backup: " . ($isMonthlyBackup ? "Yes" : "No") . "\n\n";
 /* END CONFIG QUESTIONS */
+
 
 /* DATABASE SELECTION  */
 echo "Please select at least one database from the list below:\n";
@@ -44,7 +71,6 @@ foreach ($databases as $index => $database) {
 
 echo "\n\nEnter the numbers corresponding to the databases you want to select, separated by commas:\n";
 
-$handle = fopen("php://stdin", "r");
 $input = trim(fgets($handle));
 
 $selectedIndices = explode(",", $input);
@@ -66,74 +92,81 @@ echo "\n\nYou have selected the following databases:\n";
 foreach ($selectedDatabases as $database) {
     echo "- $database\n";
 }
-
 /* END DATABASE SELECTION  */
 
 
+/* MONTHLY BACKUPS */
+if ($isMonthlyBackup) {
+    $currentDate = date("Ymd");
+
+    $dumpDir = $dumpDir . "\\MonthlyBackups";
+
+
+    $operationDumpDir = $dumpDir . "/backups_" . $currentDate . "/";
+
+    if (!is_dir($operationDumpDir)) {
+        echo "\nCreating sync directory...\n";
+        mkdir($operationDumpDir);
+    }
+
+    foreach ($selectedDatabases as $database) {
+        $currentTimestamp = date("YmdHis");
+
+        foreach ($environments as $environment) {
+            $envBackupFile = $operationDumpDir . $environment . "_backup_" . $database . "_" . $currentTimestamp . ".sql";
+            $backupCmd = generateBackupFileCommand($environment, $envBackupFile);
+            shell_exec($backupCmd);
+        }
+    }
+
+    echo "\nBackup File creation complete. Since you selected monthly backup, this process will now be terminated.\n";
+    exit(0);
+}
+/* END MONTHLY BACKUPS */
+
+
 /* SOURCE/TARGET SELECTION */
-
-
-echo "\n\nPlease select the source host for the database operation:\n";
-echo "[1] Local Host ($localDbHost)\n";
-echo "[2] Server Host ($serverDbHost)\n";
-
-echo "\nEnter the number corresponding to the source host:\n";
-$sourceHostInput = trim(fgets($handle));
-$sourceHost = '';
-$sourceUser = '';
-$sourcePassword = '';
-
-switch ($sourceHostInput) {
-    case '1':
-        $sourceHost = $localDbHost;
-        $sourceUser = $localDbUser;
-        $sourcePassword = $localDbPassword;
-        break;
-    case '2':
-        $sourceHost = $serverDbHost;
-        $sourceUser = $serverDbUser;
-        $sourcePassword = $serverDbPassword;
-        break;
-    default:
-        echo "\nInvalid selection. Please run the script again and choose a valid source host.\n";
-        exit(1);
+echo "\n\nPlease select the source environment for the database operation:\n";
+for ($i = 0; $i < count($environments); $i++) {
+    echo "[" . ($i + 1) . "] " . $environments[$i] . "\n";
 }
 
-// Select the other host option depending on what was selected as the source
-$targetHost = '';
-$targetUser = '';
-$targetPassword = '';
+echo "\nEnter the number corresponding to the source environment:\n";
+$sourceEnvInput = trim(fgets($handle));
 
-switch ($sourceHost) {
-    case $serverDbHost:
-        $targetHost = $localDbHost;
-        $targetUser = $localDbUser;
-        $targetPassword = $localDbPassword;
-        break;
-    case $localDbHost:
-        $targetHost = $serverDbHost;
-        $targetUser = $serverDbUser;
-        $targetPassword = $serverDbPassword;
-        break;
-    default:
-        echo "\nInvalid selection. Please run the script again and choose a valid target host.\n";
-        exit(1);
+if (!isset($environments[intval($sourceEnvInput) - 1])) {
+    die("Invalid environment input.");
 }
+
+$sourceEnv = $environments[intval($sourceEnvInput) - 1];
+
+echo "\n\nPlease select the target environment for the database operation:\n";
+for ($i = 0; $i < count($environments); $i++) {
+    if ($environments[$i] === $sourceEnv) {
+        continue;
+    }
+    echo "[" . ($i + 1) . "] " . $environments[$i] . "\n";
+}
+
+echo "\nEnter the number corresponding to the target environment:\n";
+$targetEnvInput = trim(fgets($handle));
+
+if (!isset($environments[intval($targetEnvInput) - 1]) || $environments[intval($targetEnvInput) - 1] === $sourceEnv) {
+    die("Invalid environment input.");
+}
+
+$targetEnv = $environments[intval($targetEnvInput) - 1];
 
 echo "\n\nYou have selected the following configurations:\n";
-echo "Source Host: $sourceHost\n";
-echo "Target Host: $targetHost\n";
-
+echo "Source Environment: $sourceEnv\n";
+echo "Target Environment: $targetEnv\n";
 echo "\nProceeding with the database operation...\n";
-
 /* END SOURCE/TARGET SELECTION */
 
 
 /* BACKUP FILE CREATION */
 $currentDate = date("Ymd");
-$sourcefileSuffix = $sourceHost === $localDbHost ? "local" : "server";
-$targetfileSuffix = $targetHost === $localDbHost ? "local" : "server";
-$operationDumpDir = $dumpDir . "/" . $sourcefileSuffix . "_to_" . $targetfileSuffix . "_" . $currentDate . "/";
+$operationDumpDir = $dumpDir . "/" . $sourceEnv . "_to_" . $targetEnv . "_" . $currentDate . "/";
 
 if (!is_dir($operationDumpDir)) {
     echo "\nCreating sync directory...\n";
@@ -144,53 +177,29 @@ $sourceBackupFiles = [];
 
 foreach ($selectedDatabases as $database) {
     $currentTimestamp = date("YmdHis");
-    $sourceBackupFile = $operationDumpDir . $sourcefileSuffix . "_backup_" . $database . "_" . $currentTimestamp . ".sql";
-    $targetBackupFile = $operationDumpDir . $targetfileSuffix . "_backup_" . $database . "_" . $currentTimestamp .  ".sql";
+    $sourceBackupFile = $operationDumpDir . $sourceEnv . "_backup_" . $database . "_" . $currentTimestamp . ".sql";
+    $targetBackupFile = $operationDumpDir . $targetEnv . "_backup_" . $database . "_" . $currentTimestamp .  ".sql";
 
     $sourceBackupFiles[$database] =  $sourceBackupFile;
 
-    function generateBackupFileCommand(string $host, string $user, string $password, string $file): string
-    {
-        global $database;
-        $escapedPassword = str_replace(" ", "!", escapeshellarg($password));
-        return "\"C:\\Program Files\\MySQL\\MySQL Workbench 8.0 CE\\mysqldump.exe\" --host=$host --port=3306 --default-character-set=utf8 --user=$user --password=$escapedPassword --protocol=tcp --skip-triggers --skip-column-statistics $database > $file";
-    }
 
-    $sourceBackupCmd = generateBackupFileCommand($sourceHost, $sourceUser, $sourcePassword, $sourceBackupFile);
-    $targetBackupCmd = generateBackupFileCommand($targetHost, $targetUser, $targetPassword, $targetBackupFile);
+    $sourceBackupCmd = generateBackupFileCommand($sourceEnv, $sourceBackupFile);
+    $targetBackupCmd = generateBackupFileCommand($targetEnv, $targetBackupFile);
 
     shell_exec($sourceBackupCmd);
     shell_exec($targetBackupCmd);
 }
 /* END BACKUP FILE CREATION */
 
+
 /* SYNC FILE CREATION */
-$jsonInfo = file_get_contents("db-info.json");
-
-if ($jsonInfo === false) {
-    die("Failed to read JSON file.");
-}
-
-$dbInfo = json_decode($jsonInfo, true);
-
-if (json_last_error() !== JSON_ERROR_NONE) {
-    die("Failed to decode JSON: " . json_last_error_msg());
-}
+$syncFiles = [];
 
 foreach ($sourceBackupFiles as $database => $sourceSyncFile) {
     if (isset($dbInfo[$database])) {
-        $jsonEntry = $dbInfo[$database];
-        $replacementUrl = "";
-        $searchUrl = "";
-        $newSyncFile = str_replace($sourcefileSuffix . "_backup", $sourcefileSuffix . "_to_" . $targetfileSuffix, $sourceSyncFile);
-
-        if ($sourcefileSuffix === "local") {
-            $replacementUrl = $jsonEntry["prodUrl"];
-            $searchUrl = $jsonEntry["localUrl"];
-        } else {
-            $replacementUrl = $jsonEntry["localUrl"];
-            $searchUrl = $jsonEntry["prodUrl"];
-        }
+        $replacementUrl = $dbInfo[$database][$targetEnv]["url"];
+        $searchUrl = $dbInfo[$database][$sourceEnv]["url"];
+        $newSyncFile = str_replace($sourceEnv . "_backup", $sourceEnv . "_to_" . $targetEnv, $sourceSyncFile);
 
         echo "\nStarting URL Replacement...\n";
 
@@ -210,6 +219,8 @@ foreach ($sourceBackupFiles as $database => $sourceSyncFile) {
             die("Failed to write to the file.");
         }
 
+        $syncFiles[$database] = $newSyncFile;
+
         echo "\nURL replacement completed successfully.\n";
     } else {
         die("No JSON entry found for database: $database\n");
@@ -218,9 +229,23 @@ foreach ($sourceBackupFiles as $database => $sourceSyncFile) {
 
 /* END SYNC FILE CREATION */
 
+
 /* SYNC */
 if ($isDryRun) {
     echo "\nFile creation complete. Since you selected dry run, this process will now be terminated.\n";
     exit(0);
 }
+
+foreach ($syncFiles as $database => $syncFile) {
+    $host = $dbInfo[$database][$targetEnv]["host"];
+    $user = $dbInfo[$database][$targetEnv]["user"];
+    $password = $dbInfo[$database][$targetEnv]["password"];
+    $escapedPassword = str_replace(" ", "!", escapeshellarg($password));
+    $syncCmd  = "\"C:\\Program Files\\MySQL\\MySQL Workbench 8.0 CE\\mysql.exe\" --host=$host --port=3306 --default-character-set=utf8 --user=$user --password=$escapedPassword --comments --database=$database  <$syncFile";
+    echo "\nSyncing $sourceEnv to $targetEnv, Database: $database\n";
+    shell_exec($syncCmd);
+}
+
+echo "\n Synchronization is complete. This process will now be terminated.\n";
+exit(0);
 /* END SYNC */
